760cd58d8081efbe5163288e04f28503233b3351
Chad Nelson <chad.nelson@temple.edu>
2020-08-21 10:02:24 -0400

    Extract and store data from table to leveldb and print json

--- extractor.py
+++ extractor.py
@@ -1,6 +1,8 @@
+from bson import dumps as bdumps
 import datetime
-import json
+from json import dumps as jdumps
 import pandas as pd
+import plyvel
 import requests
 
 
@@ -11,21 +13,29 @@ doc = requests.get(
 
 # reads all of the tables in the html into a dataframe
 # since there is only one, grab the first
-df = pd.read_html(doc)[0]
+original_df = pd.read_html(doc)[0]
 
 # The dataframe treats the first column of the table, as a data column
 # but it is actually the labels of student, staff, and total
-# so we're pop
-ddf = df.to_dict()
-groups = ddf.pop('Unnamed: 0')
+# so we're going to set that column as the index.
+# Next, we tanspose the index and columns because I was more interested in 
+# grouping groups (Students, staff) than I was about grouping locations
 
+df = ( 
+    original_df
+    .set_index(original_df.columns[0])
+    .transpose()
+    .to_dict()
+)
+
+now = datetime.datetime.now().isoformat()
 
 output_dict = {
-    "timestamp": datetime.datetime.now().isoformat(),
-    "data": {}
+    "timestamp": now,
+    "data": df
 }
 
-for index, group in groups.items():
-    output_dict["data"][group] = { k:v[index] for k,v in ddf.items()}
+db = plyvel.DB("./data", create_if_missing=True)
+db.put(now.encode('utf-8'), bdumps(df))
 
-print(json.dumps(output_dict))
+print(jdumps(output_dict))
